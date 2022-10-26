7841a2819439714e031cd2d492fd51fa82fbf7b6
John Livee Oroncillo <johnlivee.o@singlife.com>
2022-10-26 10:48:30 +0800

    added daily login notification

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const moment = require('moment-timezone');
+moment.tz.setDefault('Asia/Manila');
+
 require('dotenv/config');
 
 const express = require('express');
@@ -8,83 +11,135 @@ const DISCORD_WEBHOOK = process.env.DISCORD_WEBHOOK || '';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const init  = async (res) => {
-  if (!DISCORD_WEBHOOK || DISCORD_WEBHOOK === '') {
-    return res.status(400).send('UNABLE TO SEND WEBHOOK'); 
-  }
-  const args = process.argv.slice(2);
+const sendDiscord = async(params, res) => {
+    if (!DISCORD_WEBHOOK || DISCORD_WEBHOOK === '') {
+        return res.status(400).send('UNABLE TO SEND WEBHOOK'); 
+    }
+        
+    console.log(JSON.stringify(params, null, 2));
+
+    axios.post(DISCORD_WEBHOOK, params, {
+        headers: {
+            'Content-type': 'application/json',
+        }
+    }).then(res => {
+        const { data } = res;
+        console.log('RESPONSE', data);
+    }).catch(e => {
+        console.error(e.message, e.stack, JSON.stringify(e, null,4));
+    });
+}
+
+const dailyMission  = async (res) => {
   const url = `https://sf.bullgamez.com/`;
   try {
     const response = await axios.get(url);
     const $ = cheerio.load(response.data);
-    let missions =  $('h2:contains("Missions of The Day")');
+    const missions =  $('h2:contains("Missions of The Day")');
     // const container = $(mission).closest('li').html();
+    var params = {
+        username: "SF Bull",
+        avatar_url: "https://epins-gamestore.com/image/cache/catalog/bullsf/13239044_851643544979507_4216959116671837103_n-300x300.jpg",
+        content: "",
+        embeds: []
+    }
 
     if (missions.length) {
-        missions = [missions[0]];
-        var params = {
-            username: "SF Bull",
-            avatar_url: "https://epins-gamestore.com/image/cache/catalog/bullsf/13239044_851643544979507_4216959116671837103_n-300x300.jpg",
-            content: "",
-            embeds: []
-        }
-        for (let m = 0; m < missions.length; m++) {
-            const mission = missions[m];
-            const index = params.embeds.push({
-                "title": $(mission).text(),
-                "color": m === 0 ? 22015 : 15258703,
-                "thumbnail": {
-                    "url": "",
-                },
-                "fields": []
-            }) - 1;
+        const regular_mission = missions[0];
+        const index = params.embeds.push({
+            "title": $(regular_mission).text(),
+            "color": 22015,
+            "thumbnail": {
+                "url": "",
+            },
+            "fields": []
+        }) - 1;
 
-            const tr = $(mission).closest('li').find('table').find('tr');
-            const td = $(tr).find('td');
-            if (td.length) {
-                let child_index = -1;
-                for (let r = 1; r < tr.length; r++) {
-                    const tds = $(tr[r]).find('td');
-                    const isOdd = (r%2);
-                    if (isOdd) {
-                        let name = [];
-                        for (let t = 0; t < tds.length; t++) {
-                            name.push($(tds[t]).text());
-                        }
-                        child_index = params.embeds[index].fields.push({
-                            name: "🎯" + name.join(' - '),
-                            value: "",
-                            inline: false,
-                        }) - 1;
-                    } else if (child_index !== -1 && !isOdd) {
-                        const td = $(tr[r]).find('td');
-                        params.embeds[index].fields[child_index].value = $(td[1]).html();
-                        child_index = -1;
+        const tr = $(regular_mission).closest('li').find('table').find('tr');
+        const td = $(tr).find('td');
+        if (td.length) {
+            let child_index = -1;
+            for (let r = 1; r < tr.length; r++) {
+                const tds = $(tr[r]).find('td');
+                const isOdd = (r%2);
+                if (isOdd) {
+                    let name = [];
+                    for (let t = 0; t < tds.length; t++) {
+                        name.push($(tds[t]).text());
                     }
+                    child_index = params.embeds[index].fields.push({
+                        name: "🎯" + name.join(' - '),
+                        value: "",
+                        inline: false,
+                    }) - 1;
+                } else if (child_index !== -1 && !isOdd) {
+                    const td = $(tr[r]).find('td');
+                    params.embeds[index].fields[child_index].value = $(td[1]).html();
+                    child_index = -1;
                 }
             }
         }
 
-        console.log(JSON.stringify(params, null, 2));
-
-        axios.post(DISCORD_WEBHOOK, params, {
-            headers: {
-                'Content-type': 'application/json',
-            }
-        }).then(res => {
-            const { data } = res;
-            console.log('RESPONSE', data);
-        }).catch(e => {
-            return res.status(400).send(e.message);
-        });
-        
+        await sendDiscord(params, res);
     }
   } catch (e) {
     console.error(e);
   }
 }
+
+const dailyLogin  = async (res) => {
+    const url = `https://sf.bullgamez.com/`;
+    try {
+      const response = await axios.get(url);
+      const $ = cheerio.load(response.data);
+      const daily_logins =  $('h2:contains("Login Event on date")');
+      // const container = $(mission).closest('li').html();
+      var params = {
+          username: "SF Bull",
+          avatar_url: "https://epins-gamestore.com/image/cache/catalog/bullsf/13239044_851643544979507_4216959116671837103_n-300x300.jpg",
+          content: "",
+          embeds: []
+      }
+  
+      if (daily_logins.length) {
+          const daily_login = daily_logins[0];
+          const index = params.embeds.push({
+              "title": $(daily_login).text(),
+              "color": 15258703,
+              "thumbnail": {
+                  "url": "",
+              },
+              "fields": []
+          }) - 1;
+  
+          const tr = $(daily_login).closest('li').find('table').find('tr');
+          const span = $(tr).find('td').find('span');
+          if (span.length) {
+                const data = $(daily_login).text().split('-');
+                const date = data[1].trim().replace(/\./g, '-');
+                const formatted = moment(new Date(date)).format('YYYY-MM-DD');
+                console.log(date, new Date(date));
+                params.embeds[index].fields.push({
+                    name: "📅 Daily Login Notification",
+                    value: $(span).text(),
+                    inline: false,
+                });
+          }
+
+
+          await sendDiscord(params, res);
+      }
+    } catch (e) {
+      console.error('ERROR', e);
+    }
+}
+
 app.get('/webhook', async (req, res)=>{
-    await init(res);
+    await dailyMission(res);
+    return res.status(200).send('Webhook success');
+});
+app.get('/daily-login', async (req, res)=>{
+    await dailyLogin(res);
     res.status(200).send('Webhook success');
 });
 
